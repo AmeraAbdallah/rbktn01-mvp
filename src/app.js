9d9fa974c0854ae99b327301f02ed7ddf253be19
Amera Ayad <amera.ayad.it@gmail.com>
2020-01-03 23:20:28 +0100

    fix Access-Control-Allow-Origin error

--- src/app.js
+++ src/app.js
@@ -12,6 +12,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.static(publicPath));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, auth");
+    next();
+});
 app.use('/api/user', userRouter);
 
 
