fa3d714d8e47f03cd99352792a4ef3e9d34be328
Amera Ayad <amera.ayad.it@gmail.com>
2020-01-03 23:15:23 +0100

    fixing private route

--- client/src/routers/PrivateRoute.js
+++ client/src/routers/PrivateRoute.js
@@ -1,22 +1,30 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { Route, Redirect } from 'react-router-dom'
 
-const PrivateRoute = ({
-    auth,
-    component,
-    ...rest
-}) => (
-    <Route {...rest} component = {(props) => (
-        auth ? (
-            <div>
-                <Component {...props}/>
-            </div>
-        ):(
-            <Redirect to = '/' />
-        )
+// const PrivateRoute = ({
+//     auth,
+//     component,
+//     ...rest
+// }) => (
+//     <Route {...rest} component = {(props) => (
+//         auth ? (
+//             <div>
+//                 <Component {...props}/>
+//             </div>
+//         ):(
+//             <Redirect to = '/' />
+//         )
+//     )} />
+// )
+
+const PrivateRoute = ({ component: Component,  auth, ...rest }) => (
+    <Route {...rest} render={(props) => (
+        auth 
+        ? <Component {...props} />
+        : <Redirect to='/' />
     )} />
-)
+  )
 
 const mapStateToProps = (state) => ({
     auth: !!state.user._id
