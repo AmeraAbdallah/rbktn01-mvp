ba495d961f60c3ec2e1852c7c844367415c860ea
Amera Ayad <amera.ayad.it@gmail.com>
2020-01-04 04:52:49 +0100

    use todos in home

--- client/src/components/Home/home.js
+++ client/src/components/Home/home.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import TodoForm from './../TodoForm/todoForm';
 import Header from './../Header/header';
 
 export class home extends Component {
+  
   render() {
+    console.log(this.props.todos)
     return (
       <div>
         <Header/>
-        <p>wfwe</p>
+        <TodoForm/>
+        <div>
+        {
+          this.props.todos.map((todo, i) => <div key = {i}>{todo.title}</div>)
+        }
+        </div>
       </div>
     )
   }
 }
 
 const mapStateToProps = (state) => ({
-  
+  todos: state.todos
 })
 
 const mapDispatchToProps = {
